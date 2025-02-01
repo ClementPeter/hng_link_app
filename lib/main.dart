0c40e14badecdcb474b1f7889dfe2737b3ba651d
ClementPeter <clementpeter2000@gmail.com>
2025-02-01 20:09:29 +0100

    feat: Add url_launcher dependency and implement hyperlink functionality in the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'HNG Hyperlink',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'HNG Hyperlink'),
     );
   }
 }
@@ -38,6 +39,14 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  final Uri _url = Uri.parse('https://flutter.dev');
+
+  Future<void> _launchURL(String url) async {
+    if (!await launchUrl(Uri.parse(url))) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,11 +58,26 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Links to various hyperlinks',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () =>
+                  _launchURL('https://github.com/ClementPeter/hng_link_app'),
+              child: const Text('GitHub Repo'),
+            ),
+            ElevatedButton(
+              onPressed: () =>
+                  _launchURL('https://hng.tech/hire/flutter-developers'),
+              child: const Text('Hire Flutter Developers'),
+            ),
+            ElevatedButton(
+              onPressed: () => _launchURL('https://telex.im/'),
+              child: const Text('Telex IM'),
+            ),
+            ElevatedButton(
+              onPressed: () => _launchURL('https://delve.fun/'),
+              child: const Text('Delve Fun'),
             ),
           ],
         ),
